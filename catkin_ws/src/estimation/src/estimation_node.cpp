1a54786fc515c68710db549c39ab12bb3d4e4eba
realWilliamWells <williamwells2013@hotmail.com>
2021-01-03 00:10:39 -0400

    Fully implement Kalman filter for x direction

--- catkin_ws/src/estimation/src/estimation_node.cpp
+++ catkin_ws/src/estimation/src/estimation_node.cpp
@@ -14,37 +14,40 @@ ros::Publisher estimation_publisher;
 // Initial parameters for filter
 float lastTime = 0;
 
-Eigen::MatrixXf I(2,1);
+Eigen::MatrixXf I(2,2);
 
-Eigen::MatrixXf processVariance(2,1);
+Eigen::MatrixXf processVariance(2,2);
 
-Eigen::MatrixXf measureVariance(2,1);
+Eigen::MatrixXf measureVariance(2,2);
 
 // Initial state
-Eigen::MatrixXf stateX(1,1);
+Eigen::MatrixXf stateX(2,1);
 
-Eigen::MatrixXf H(1,2);
+Eigen::MatrixXf H(2,2);
 
 // Initial error covariance
-Eigen::MatrixXf errorCovarianceX(2,1);
+Eigen::MatrixXf errorCovarianceX(2,2);
 
 // Equations for filter predict stage
 Eigen::MatrixXf getStateTransitionMatrix(float deltaTime) {
-    Eigen::MatrixXf stateTransition(1,2);
+    Eigen::MatrixXf stateTransition(2,2);
     stateTransition(0,0) = 1.00;
     stateTransition(0,1) = deltaTime;
+    stateTransition(1,0) = 0.00;
+    stateTransition(1,1) = 1;
 
     return stateTransition;
 }
 
 Eigen::MatrixXf getControlMatrix(float deltaTime) {
-    Eigen::MatrixXf controlMatrix(1,1);
+    Eigen::MatrixXf controlMatrix(2,1);
     controlMatrix(0,0) = 0.5 * pow(deltaTime, 2);
+    controlMatrix(1,0) = deltaTime;
 
     return controlMatrix;
 }
 
-Eigen::MatrixXf getVector(float value) {
+Eigen::MatrixXf getControlVector(float value) {
     Eigen::MatrixXf controlVector(1,1);
     controlVector(0,0) = value;
 
@@ -62,43 +65,56 @@ void predictErrorCovariance(Eigen::MatrixXf stateTransition) {
 void kalmanPredict(float deltaTime, float acceleration) {
     Eigen::MatrixXf stateTransition = getStateTransitionMatrix(deltaTime);
     Eigen::MatrixXf controlMatrix = getControlMatrix(deltaTime);
-    Eigen::MatrixXf controlVector = getVector(acceleration);
+    Eigen::MatrixXf controlVector = getControlVector(acceleration);
 
     predictState(controlMatrix, controlVector, stateTransition);
 
-    std::cout<<"test";
-
     predictErrorCovariance(stateTransition);
+
+    ROS_INFO("Test0");
 }
 
 // Equations for filter update stage
+Eigen::MatrixXf getMeasurementVector(float position, float velocity) {
+    Eigen::MatrixXf measurementVector(2,1);
+    measurementVector(0,0) = position;
+    measurementVector(1,0) = velocity;
+
+    return measurementVector;
+}
+
 Eigen::MatrixXf getKalmanGain() {
-    return stateX * H.transpose() * (H * stateX * H.transpose() + measureVariance).inverse();
+    return errorCovarianceX * H.transpose() * (H * errorCovarianceX * H.transpose() + measureVariance).inverse();
 }
 
 void updateEstimate(Eigen::MatrixXf kalmanGain, Eigen::MatrixXf measurementVector) {
-    stateX = stateX + kalmanGain * (measurementVector - H*errorCovarianceX);
+    stateX = stateX + kalmanGain * (measurementVector - H*stateX);
 }
 
 void updateErrorCovariance(Eigen::MatrixXf kalmanGain) {
     errorCovarianceX = (I - kalmanGain * H) * errorCovarianceX;
 }
 
-void kalmanUpdate(float position) {
+void kalmanUpdate(float position, float velocity) {
     Eigen::MatrixXf kalmanGain = getKalmanGain();
-    Eigen::MatrixXf measurementVector = getVector(position);
+    Eigen::MatrixXf measurementVector = getMeasurementVector(position, velocity);
 
     updateEstimate(kalmanGain, measurementVector);
+
+    ROS_INFO("Test1");
+
     updateErrorCovariance(kalmanGain);
+
+    ROS_INFO("Test2");
 }
 
 // Main kalman function
-void kalmanFilter(float position, float acceleration, float time) {
+void kalmanFilter(float position, float velocity, float acceleration, float time) {
     float deltaTime = time - lastTime;
 
     kalmanPredict(deltaTime, acceleration);
 
-    kalmanUpdate(position);
+    kalmanUpdate(position, velocity);
 
     lastTime = time;
 }
@@ -123,7 +139,7 @@ void accelerometerCallback(const dynamics_simulator::true_dynamics& msg) {
         accelerometer_msg = msg;
 
         // X direction
-        kalmanFilter(gps_msg.xPosition, accelerometer_msg.xPosition, accelerometer_msg.time);
+        kalmanFilter(gps_msg.xPosition, gps_msg.xVelocity, accelerometer_msg.xPosition, accelerometer_msg.time);
         estimated_msg.xPosition = stateX(0,0);
         estimated_msg.time = accelerometer_msg.time;
 
@@ -136,23 +152,35 @@ void accelerometerCallback(const dynamics_simulator::true_dynamics& msg) {
 int main(int argc, char **argv) {
     // Initial parameters for filter
     I(0,0) = 1.00;
+    I(0,1) = 0.00;
     I(1,0) = 0.00;
+    I(1,1) = 1.00;
 
     processVariance(0,0) = pow(0.1,2);
+    processVariance(0,1) = 0.00;
     processVariance(1,0) = 0.00;
+    processVariance(1,1) = pow(0.1,2);
+
 
     measureVariance(0,0) = pow(60.00,2);
+    measureVariance(0,1) = 0.00;
     measureVariance(1,0) = 0.00;
+    measureVariance(1,1) = pow(60.00,2);
 
-    Eigen::MatrixXf stateX(1,1);
+    Eigen::MatrixXf stateX(2,1);
     stateX(0,0) = 0.00; // Position
+    stateX(1,0) = 0.00; // Velocity
 
     H(0,0) = 1.00;
     H(0,1) = 0.00;
+    H(1,0) = 0.00;
+    H(1,1) = 1.00;
 
     // Initial error covariance
     errorCovarianceX(0,0) = 1000.00;
+    errorCovarianceX(0,1) = 0.00;
     errorCovarianceX(1,0) = 0.00;
+    errorCovarianceX(1,1) = 1000.00;
 
     // Initialize node
     ros::init(argc, argv, "accelerometer_simulator_node");
